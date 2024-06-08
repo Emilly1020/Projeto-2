15e451225bf6acf84cbd3b6867e72eb7dc07194f
Emilly <emillysantos013673@gmail.com>
2024-06-08 12:30:09 -0300

    Projeto Proz
    Projeto 2

---
+++ script.js
@@ -0,0 +1,9 @@
+let titulo = document.getElementById('titulo');
+let listaNaoOrdenada = document.querySelector('ul');
+let link = document.getElementById('link');
+let listaOrdenada = document.getElementById('lista-ordenada');
+
+titulo.innerText = 'Projeto II';
+link.innerText = 'Link para Proz Educação';
+listaNaoOrdenada.innerHTML = '<li>Proz</li><li>Educação</li><li>JOY</li>';
+listaOrdenada.innerHTML = '<li><a href="https://www.youtube.com/">Acesse o YouTube</a></li><li><a href="https://www.avon.com.br/">Acesse a Avon</a></li><li><a href="https://www.mercadolivre.com.br/">Acesse o Mercado Livre</a></li>';
